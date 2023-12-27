105c9e6a882f2e1deef555c199b42effd45376c1
doma <git4doma@gmail.com>
2023-12-28 02:25:16 +0530

    template for svelte

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main class="mainDiv">
+  <h1>ROHAN</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+
+
+  <!-- why are they using the anchor tag rather then link tag... -->
+  <!-- <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p> -->
+
+</main>
+
+<style>
+  .mainDiv {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 580px;
+  }
+
+
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,31 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+
+  const decrement = () => {
+    count -= 1
+  }
+</script>
+
+<div class="showCount">
+  <button on:click={decrement}>
+    -
+  </button>
+  
+  <div class="count">
+    {count}
+  </div>
+  
+  <button on:click={increment}>
+    +
+  </button>
+</div>
+
+<style>
+  .showCount {
+    display: flex;
+    gap: 10px;
+  }
+</style>
